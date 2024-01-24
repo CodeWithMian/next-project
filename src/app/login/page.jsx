a58b038390e4c4f0ef31e63c1f91e800ef9b04f9
Bilal <mb03086350364@gmail.com>
2024-01-24 23:45:20 +0500

    e-commerce-project

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,11 +1,56 @@
 "use client";
 import InputComponent from "@/components/FormElements/InputComponent";
+import { GlobalContext } from "@/context";
+import { login } from "@/services/login";
 import { loginFormControls } from "@/utils";
+import Cookies from "js-cookie";
 import { useRouter } from "next/navigation";
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 const Login = () => {
   const router = useRouter();
+
+  const initialFormdata = {
+    email: "",
+    password: "",
+  };
+  const [formData, setFormData] = useState(initialFormdata);
+  const {
+    isAuthUser,
+    setIsAuthUser,
+    user,
+    setUser,
+  } = useContext(GlobalContext);
+
+  function isValidForm() {
+    return formData &&
+      formData.email &&
+      formData.email.trim() !== "" &&
+      formData.password &&
+      formData.password.trim() !== ""
+      ? true
+      : false;
+  }
+
+  async function handleLogin(){
+    const res = await login(formData)
+    console.log(res)
+
+    if(res.success){ setIsAuthUser(true);
+      setUser(res?.finalData?.user);
+      setFormData(initialFormdata);
+      Cookies.set("token", res?.finalData?.token);
+      localStorage.setItem("user", JSON.stringify(res?.finalData?.user));
+    }else{
+      setIsAuthUser(false)
+    }
+  }
+  console.log(isAuthUser,user)
+
+  useEffect(() => {
+    if (isAuthUser) router.push("/");
+  }, [isAuthUser]);
+
   return (
     <div className="bg-white relative">
       <div className="flex flex-col items-center justify-between pt-0 pr-10 pb-0 pl-10 mt-8 mr-auto xl:px-5 lg:flex-row">
@@ -22,14 +67,23 @@ const Login = () => {
                       type={controlItem.type}
                       placeholder={controlItem.placeholder}
                       label={controlItem.label}
+                      value={formData[controlItem.id]}
+                      onChange={(event) => {
+                        setFormData({
+                          ...formData,
+                          [controlItem.id]: event.target.value,
+                        });
+                      }}
                     />
                   ) : null
                 )}
                 <button
-                  className="inline-flex w-full items-center justify-center bg-black px-6 py-4 text-lg 
+                  className="disabled:opacity-50 inline-flex w-full items-center justify-center bg-black px-6 py-4 text-lg 
                 text-white transition-all duration-200 ease-in-out focus:shadow font-medium uppercase tracking-wide
                 "
-                >
+               disabled={!isValidForm()}
+               onClick={handleLogin}
+               >
                   Login
                 </button>
                 <div className="flex flex-col gap-2">
